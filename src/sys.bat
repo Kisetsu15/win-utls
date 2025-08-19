f2a648a55df238ace1ced9956790aaf760f3e8b6
Dharshik <dharshik06@gmail.com>
2025-08-19 23:05:26 +0530

    Add void termination function

--- src/sys.bat
+++ src/sys.bat
@@ -1,5 +1,8 @@
 @echo off
 
+if not "%1"=="" goto main
+else goto void
+
 :main
 REM Check parameter and jump to the corresponding label
 if "%1"=="/s" goto shutdown
@@ -8,36 +11,36 @@ if "%1"=="/sl" goto sleep
 if "%1"=="/e" goto explorer
 if "%1"=="/downloads" goto downloads
 if "%1"=="/w" goto web
-goto x
+goto void
 
 :shutdown
 REM Shutdown with optional timer (default 0)
 set /a timer=0
 if not "%2"=="" set /a timer=%2  
 shutdown /s /f /t %timer%
-goto x
+goto void
 
 :restart
 REM Restart immediately
 shutdown /r /f /t 0
-goto x
+goto void
 
 :sleep
 REM Sleep for given seconds or default to 5
 set /a timer=5
 if not "%2"=="" set /a timer=%2
 timeout /t %timer%
-goto x
+goto void
 
 :explorer
 REM Open Windows Explorer
 explorer
-goto x
+goto void
 
 :downloads
 REM Open Downloads Folder
 explorer "%USERPROFILE%\Downloads"
-goto x
+goto void
 
 :help
 REM Show Help options
@@ -47,16 +50,16 @@ echo /sl [timer]  sleep
 echo /e           Explorer
 echo /downloads   Downloads
 echo /w <url>     Website
-goto x
+goto void
 
 :web
 REM Open Specified Website
 if "%2"=="" (
-    echo fatal: Url not specified
-    goto x
+  echo fatal: Url not specified
+  goto void
 )
 start "" "https://www.%2"
-goto x
+goto void
 
 REM End of script label
-:x
+:void
