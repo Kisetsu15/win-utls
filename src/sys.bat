88e963540eb417188c8c6094ca8ff3357ebeb8b2
Dharshik <dharshik06@gmail.com>
2025-08-29 17:01:56 +0530

    feat: Added restart to bios

--- src/sys.bat
+++ src/sys.bat
@@ -3,9 +3,11 @@
 if not "%1"=="" goto commnad_executor
 else goto help
 
-:commnad_executor REM Check parameter and jump to the corresponding label
+:: Check parameter and jump to the corresponding label
+:commnad_executor 
 if "%1"=="/s" goto shutdown
 if "%1"=="/r" goto restart
+if "%1"=="/b" goto bios
 if "%1"=="/sl" goto sleep
 if "%1"=="/e" goto explorer
 if "%1"=="/downloads" goto downloads
@@ -13,58 +15,61 @@ if "%1"=="/w" goto web
 if "%1"=="/h" goto help
 goto void
 
-:shutdown REM Shutdown with optional timer (default 0)
+:: Shutdown with optional timer (default 0)
+:shutdown 
 set /a timer=0
 if not "%2"=="" set /a timer=%2
 shutdown /s /f /t %timer%
 goto void
 
-:restart REM Restart immediately
+:: Restart immediately
+:restart 
 shutdown /r /f /t 0
 goto void
 
-:sleep REM Sleep for given seconds or default to 5
+:: Restart into BIOS
+:bios 
+shutdown /r /fw /f /t 0
+goto void
+
+:: Sleep for given seconds or default to 5
+:sleep 
 set /a timer=5
 if not "%2"=="" set /a timer=%2
 timeout /t %timer%
 goto void
 
-:explorer REM Open Windows Explorer
-if "%2"=="" set dir="."
+:: Open Windows Explorer
+:explorer 
+if "%1"=="" set dir="."
 if "%2"=="home" set dir=""
 explorer %dir%
 goto void
 
-:downloads REM Open Downloads Folder
+:: Open Downloads Folder
+:downloads 
+explorer "%USERPROFILE%\Downloads"
+goto void
 
-:help REM Show Help options
+:: Show Help options
+:help 
 echo /s [timer]     Shutdown
 echo /r             Restart
+echo /b             BIOS
 echo /sl [timer]    Sleep
 echo /e  [address]  Explorer
 echo /downloads     Downloads
-echo /w [/ip] <url>       Website
+echo /w <url> 	    Website
 goto void
 
-:web REM Open Specified Website
-if "%2"=="/ip" (
-  goto ip_address
-)
+:: Open Specified Website
+:web
 if "%2"=="" (
   echo fatal: Url not specified
   goto void
 )
-start "" "https://www.%2"
+start "" "https://%2"
 goto void
 
-
-:ip_address
-if "%3"=="" (
-  echo fatal: IP address not specified
-  goto void
-)
-start "" "https://%3"
-goto void
-
-REM End of script label
+:: End of script label
 :void
