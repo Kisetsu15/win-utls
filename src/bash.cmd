2884c76cdb3ea4071f3561af80d60b3ca3b7222c
Dharshik <dharshik06@gmail.com>
2025-09-08 18:49:49 +0530

    Input safety

--- src/bash.cmd
+++ src/bash.cmd
@@ -5,9 +5,9 @@ title Bash
 :Loop
 echo.
 echo !ESC![36m%USERNAME%@%COMPUTERNAME% !ESC![33m~ !ESC![32m%CD%!ESC![0m
-SET /P input=$ 
+set /P "input=$ "
 if "%input%"=="q" endlocal & goto :eof
 if "%input%"=="Q" endlocal & goto :eof
-if NOT "%input%"=="" powershell -command "%input%"
+if not "%input%"=="" powershell -command "%input%"
 goto Loop
 
